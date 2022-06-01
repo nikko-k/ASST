d3854d044d1ef9db5075e6d81f0454ee5c81d086
Nikolay Kolev <nikolay.kolev.04@gmail.com>
2022-06-01 17:54:57 +0300

    Add command line argument parsing

--- main.js
+++ main.js
@@ -4,17 +4,25 @@ process.stdout.write('\033c'); // Clear screen
 
 // Imports
 const config = require("./config.js");
+const yargs = require("yargs/yargs");
+const { hideBin } = require('yargs/helpers');
+
+const argv = yargs(hideBin(process.argv)).argv
+
 var start = "";
 
 // Global Variables
 // none for now
+
+config.DEFAULT_PROJECT_PATH_TO_SCAN = argv.path || argv.p || config.DEFAULT_PROJECT_PATH_TO_SCAN;
+config.HTML_REPORT_FILE_NAME_AND_PATH = argv.report || argv.r || config.HTML_REPORT_FILE_NAME_AND_PATH;
 
 // Main
 if(config.USED_PROGRAMMING_LANGUAGE.toLowerCase() == "php"){
 	start = require("./langs/php");
 }
 else if(config.USED_PROGRAMMING_LANGUAGE.toLowerCase() == "c#"){
-	//start = require("./langs/c#");
+	start = require("./langs/c#");
 }
 else {
 	console.log();
